83c7558164624fa1ff56f38b0bc8c243a0496ee3
Tom Pollard <tom.pollard.11@ucl.ac.uk>
2015-04-27 10:26:49 -0400

    add contribution page

--- _posts/2015-04-23-contributing.md
+++ _posts/2015-04-23-contributing.md
@@ -5,4 +5,28 @@ category: bac
 date: 2015-04-23 13:32:16
 ---
 
+#### How to contribute
+
+Providing support for the MIMIC-II database is an ongoing process. Our resources are limited, so please help us to update and improve documentation.
+
+These guidelines are hosted on GitHub, making it straightforward for you to raise issues and suggest changes.
+
+#### Raising an issue
+
+If you notice something that could be improved, such as:
+
+- a factual error in the guidelines;
+- a common question that should be documented;
+
+...then please raise an issue on Github. To raise an issue, [open the issue tracker](https://github.com/mimic2/v3.0/issues), select "New issue", and then add a description (along with a suggested solution, if possible).
+
+#### Updating the guidelines
+
+We encourage you to directly contribute to the guidelines. To do this, please:
+
+- fork [the repository](https://github.com/mimic2/v3.0)
+- edit the documentation as appropriate
+- submit a pull request 
+
+
 
--- code/mimic2v30_beta_utc.sql
+++ code/mimic2v30_beta_utc.sql
@@ -15,7 +15,7 @@ Recreating mimic2v30 with new updates
 
 --conversion of time zones
 --drop table mimic2v30b_utc.additives;
---create table mimic2v30b_utc.additives as
+create table mimic2v30b_utc.additives as
 select 
 SUBJECT_ID
 ,ICUSTAY_ID
@@ -42,21 +42,9 @@ from mimic2v30b.additives;
 
 /***************    admissions  ********************/
 
-/* create table mimic2v30b_utc.admissions as
-select * from mimic2v30b.admissions;
-
-update mimic2v30b_utc.admissions a 
-set ADMIT_TIME=
-(select cast(ADMIT_TIME AT TIME ZONE 'UTC' as timestamp) as ADMIT_TIME 
-from mimic2v30b_utc.admissions b where a.ADMISSIONSDATAID=b.ADMISSIONSDATAID) 
-where ADMIT_TIME is not null;
-
-rollback;
-commit; */
-
 --conversion of time zones
 --drop table mimic2v30b_utc.admissions;
---create table mimic2v30b_utc.admissions as
+create table mimic2v30b_utc.admissions as
 select
 HADM_ID,
 SUBJECT_ID,
@@ -73,6 +61,7 @@ from mimic2v30b.admissions;
 
 /***************    censusevents  ********************/
 
+-- TP: Mornin's version used (not recreated)
 create table mimic2v30b_utc.censusevents as
 select 
 CENSUS_ID
@@ -88,51 +77,98 @@ CENSUS_ID
 from mimic2v30b.censusevents;
 
 
-
 /***************    chartevents  ********************/
-
+create table mimic2v30b_utc.chartevents as
+select
+SUBJECT_ID
+,HADM_ID
+,ICUSTAY_ID
+,ITEMID
+,LABEL
+,cast(TIME AT TIME ZONE 'UTC' as timestamp) as TIME
+,ELEMID
+,cast(VALIDATIONTIME AT TIME ZONE 'UTC' as timestamp) as KEYINTIME
+,CGID
+,CUID
+,VALUE1
+,VALUE1NUM
+,VALUE1UOM
+,COMMENTS
+,VALUE2
+,VALUE2NUM
+,VALUE2UOM
+,RESULTSTATUS
+,STOPPED
+,WARNING
+,ERROR
+,CHARTEVENTSDATAID
+from mimic2v30b.chartevents;
 
 /***************    cptevents  ********************/
+create table mimic2v30b_utc.cptevents as
+select
 
+from mimic2v30b.cptevents;
 
 /***************    D_CAREGIVERS  ********************/
-
-
+create table mimic2v30b_utc.D_CAREGIVERS as
+select
+from mimic2v30b.D_CAREGIVERS;
 
 /***************    D_CAREUNITS  ********************/
-
-
+create table mimic2v30b_utc.D_CAREUNITS as
+select
+from mimic2v30b.D_CAREUNITS;
 
 /***************    d_items  ********************/
-
-
+create table mimic2v30b_utc.d_items as
+select
+from mimic2v30b.d_items;
 
 /***************    D_PATIENTS  ********************/
-
+create table mimic2v30b_utc.D_PATIENTS as
+select
+from mimic2v30b.D_PATIENTS;
 
 /***************    D_UNITS  ********************/
-
+create table mimic2v30b_utc.D_UNITS as
+select
+from mimic2v30b.D_UNITS;
 
 /***************    DEMOGRAPHIC_DETAIL  ********************/
-
+create table mimic2v30b_utc.DEMOGRAPHIC_DETAIL as
+select
+from mimic2v30b.DEMOGRAPHIC_DETAIL;
 
 /***************    DRGEVENTS  ********************/
-
+create table mimic2v30b_utc.DRGEVENTS as
+select
+from mimic2v30b.DRGEVENTS;
 
 /***************    ICD9  ********************/
-
+create table mimic2v30b_utc.ICD9 as
+select
+from mimic2v30b.ICD9;
 
 /***************    ICUSTAY_DAYS  ********************/
-
+create table mimic2v30b_utc.ICUSTAY_DAYS as
+select
+from mimic2v30b.ICUSTAY_DAYS;
 
 /***************    ICUSTAYEVENTS  ********************/
-
+create table mimic2v30b_utc.ICUSTAYEVENTS as
+select
+from mimic2v30b.ICUSTAYEVENTS;
 
 /***************    IOEVENTS  ********************/
-
+create table mimic2v30b_utc.IOEVENTS as
+select
+from mimic2v30b.IOEVENTS;
 
 /***************    labevents  ********************/
-
+create table mimic2v30b_utc.labevents as
+select
+from mimic2v30b.labevents;
 
 /***************    MEDEVENTS  ********************/
 select
@@ -159,7 +195,9 @@ SUBJECT_ID
 from mimic2v30b.MEDEVENTS;
 
 /***************    MICROBIOLOGYEVENTS  ********************/
-
+create table mimic2v30b_utc.MICROBIOLOGYEVENTS as
+select 
+from mimic2v30b.MICROBIOLOGYEVENTS;
 
 /***************    NOTEEVENTS  ********************/
 create table mimic2v30b_utc.NOTEEVENTS as
@@ -183,9 +221,12 @@ REC_ID
 from mimic2v30b.NOTEEVENTS;
 
 /***************    ORDERENTRY  ********************/
-
+create table mimic2v30b_utc.NOTEEVENTS as
+select 
+from mimic2v30b.NOTEEVENTS;
 
 /***************    POE_MED_ORDER  ********************/
+-- TP: Mornin's version used (not recreated)
 -- remove drug_name_poe column
 -- converted timezone and renamed start_dt and stop_dt to starttime and endtime
 create table mimic2v30b_utc.POE_MED_ORDER as
@@ -301,10 +342,17 @@ SUBJECT_ID
 from mimic2v30b.TOTALBALEVENTS;
 
 /***************    ventilation  ********************/
-
+create table mimic2v30b_utc.ventilation as
+select 
+from mimic2v30b.ventilation;
 
 /***************    lcp_COMORBIDITY_SCORES  ********************/
-
-
+create table mimic2v30b_utc.lcp_COMORBIDITY_SCORES as
+select 
+from mimic2v30b.lcp_COMORBIDITY_SCORES;
 
 /***************    lcp_daily_sofa  ********************/
+create table mimic2v30b_utc.lcp_daily_sofa as
+select 
+from mimic2v30b.lcp_daily_sofa;
+
